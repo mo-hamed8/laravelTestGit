0d1a02f18dfd07ecc5205b39ecfd4c5da93eda98
mo.hamed <mohamme5hame5@gmail.com>
2026-02-12 04:18:25 +0300

    Create hello page

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::view('hello', 'helloPage');
+
 require __DIR__.'/auth.php';
